8c6434540d141b35c250b1b3a2dfe13007635591
dengqunhua_123@163.com <dengqunhua_123@163.com>
2019-03-09 19:25:52 +0800

    代码提交

---
+++ src/main/java/schema/builder/Persion.java
@@ -0,0 +1,36 @@
+package schema.builder;
+
+public class Persion {
+	
+	private String username;
+	private String sex;
+	private int age;
+	private String adderess;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getAdderess() {
+		return adderess;
+	}
+	public void setAdderess(String adderess) {
+		this.adderess = adderess;
+	}
+	
+	
+
+}
